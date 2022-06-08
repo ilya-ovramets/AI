77b3193ffef26ce54f9824d6d382c0caab24c17d
Elay <ovramecilla@gmail.com>
2022-06-08 23:17:15 +0300

    Create new branche

---
+++ FotoReader/FotoReader/NeyronType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoReader
+{
+    enum NeyronType
+    {
+        Input = 0,
+        Normal = 1,
+        Output = 2,
+        
+    }
+}
